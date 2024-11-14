be41af5b0aeb5b9963ebc396e10fa5e4a14b2f23
Wilfred <wilfredkang@live.com>
2024-11-15 06:37:26 +0800

    Update function.h
    Update with sort

--- function.h
+++ function.h
@@ -3,9 +3,12 @@
 #include <vector>
 #include "task.h"
 
-void printTasks(std::vector<Task> vec);
+void printTasks(std::vector<Task>& vec);
 bool createTask(std::vector<Task>& vec);
 void updateStatus(std::vector<Task>& vec);
 void deleteTask(std::vector<Task>& vec);
+void sortTasks(std::vector<Task>& vec);
 
+void displayTasks(std::vector<Task>& vec);
 bool verifyDate(std::string s);
+bool compareDate(Task t1, Task t2);
